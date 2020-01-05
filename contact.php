f24cac66149218e70022aae6e460bcd4c29cc8f2
eugenesinamban <49954579+eugenesinamban@users.noreply.github.com>
2020-01-05 23:26:39 +0900

    Delete .gitattributes

---
+++ contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once(__DIR__ . "/include/head.html") ?>
+</head>
+<body>
+    <header>
+        <?php require_once(__DIR__ . "/include/header.html"); ?>
+    </header>
+    <main>
+        <section class="hero is-light is-medium">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">
+                        Contact
+                    </h1>
+                    <h2 class="subtitle is-3">
+                        Get in touch with Eugene
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="hero is-dark is-large">
+            <div class="hero-body">
+                <div class="container">
+                    <h3 class="title is-4 has-text-white has-text-centered">
+                        <strong>Contact Details</strong>
+                    </h3>
+                    <h4 class="is-6 has-text-white has-text-centered">
+                        Email Address : eugene.sinamban@gmail.com
+                    </h4>
+                    <h4 class="is-6 has-text-white has-text-centered">
+                        Cellphone : 080-2150-3097
+                    </h4>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="footer">
+        <div class="container">
+            <?php require_once(__DIR__ . "/include/footer.html"); ?>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once(__DIR__ . "/include/head.html") ?>
+</head>
+<body>
+    <header>
+        <?php require_once(__DIR__ . "/include/header.html"); ?>
+    </header>
+    <main>
+        <section class="hero is-light is-medium">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">
+                        Portfolio
+                    </h1>
+                    <h2 class="subtitle is-3">
+                        Eugene's works so far
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="tile is-ancestor">
+                <div class="tile is-12">
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <a href="../genki/top.html">
+                                <figure class="image is-5by3">
+                                    <img src="images/Genki.png">
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                    <div class="tile is-parent is-vertical is-4">
+                        <article class="tile is-child">
+                            <p class="title is-1 has-text-white">
+                                元気モリモリご飯パワー
+                            </p>
+                        </article>
+                        <article class="tile is-child">
+                            <p class="title is-2 has-text-white">
+                                My very first website, written in Japanese, created to use during self introductions
+                            </p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="line"></div>
+        <section class="section">
+            <div class="tile is-ancestor">
+                <div class="tile is-12">
+                    <div class="tile is-parent is-vertical is-4">
+                        <article class="tile is-child">
+                            <p class="title is-1 has-text-white">
+                                Yogi's Exchange
+                            </p>
+                            <!-- <a class="subtitle is-3 has-text-white" href="http://yogiexchange.rf.gd">
+                                http://yogiexchange.rf.gd/
+                            </a> -->
+                        </article>
+                        <article class="tile is-child">
+                            <p class="title is-2 has-text-white">
+                                Website to check foreign exchange rates
+                            </p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <a href="http://yogiexchange.rf.gd/">
+                            <!-- <a href="../exchangeapp"> -->
+                                <figure class="image is-5by3">
+                                    <img src="images/Exchange.png">
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="line"></div>
+        <section class="section">
+            <div class="tile is-ancestor">
+                <div class="tile is-12">
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <a href="http://organizer.epizy.com">
+                            <!-- <a href="../organizer/index/index.php"> -->
+                                <figure class="image is-5by3">
+                                    <img src="images/Organizer.png">
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                    <div class="tile is-parent is-vertical is-4">
+                        <article class="tile is-child">
+                            <p class="title is-1 has-text-white">
+                                Organizer
+                            </p>
+                            <!-- <a href="http://organizer.epizy.com" class="subtitle is-3 has-text-white">
+                                http://organizer.epizy.com
+                            </a> -->
+                        </article>
+                        <article class="tile is-child">
+                            <p class="title is-3 has-text-white">
+                                Organizer created for foreigners living in Japan, especially students like me,
+                                who need to organizer their Part-time job hours and keep track of salary which is
+                                checked by the Immigration Office of Japan
+                            </p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="line"></div>
+        <section class="section">
+            <div class="tile is-ancestor">
+                <div class="tile is-12">
+                    <div class="tile is-parent is-vertical is-4">
+                        <article class="tile is-child">
+                            <p class="title is-1 has-text-white">
+                                Tokyo Readers Club
+                            </p>
+                        </article>
+                        <article class="tile is-child">
+                            <p class="title is-3 has-text-white">
+                                Organizer created for foreigners living in Japan, especially students like me,
+                                who need to organizer their Part-time job hours and keep track of salary which is
+                                checked by the Immigration Office of Japan
+                            </p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <a href="../tokyo-readers-club/index.php">
+                                <figure class="image is-5by3">
+                                    <img src="images/ReadersClub.png">
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="footer">
+        <div class="container">
+            <?php require_once(__DIR__ . "/include/footer.html"); ?>
+        </div>
+    </footer>
+</body>
+</html>
